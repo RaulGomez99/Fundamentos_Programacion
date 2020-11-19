2063a7f60fc23e766766374a8164f2abbb8dd760
RaulGomez99 <55094646+RaulGomez99@users.noreply.github.com>
2020-11-19 23:20:44 +0100

    Creo que está mejor

--- Tema 5/EjercicioTaylor.py
+++ Tema 5/EjercicioTaylor.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov 19 22:47:40 2020
-
+Calcula el taylor de e^x
 @author: Raul
 """
+from math import e
 
 def Factorial(num:int)->int:
     if num == 0 or num == 1:
@@ -12,19 +13,20 @@ def Factorial(num:int)->int:
         retorna = Factorial(num - 1)
     return retorna
 
-def Taylor(x:int, cantidad_terminos:int)->int:
+def Taylor(x:int, cantidad_terminos:int, a:int)->int:
     cont = 0
     for i in range(cantidad_terminos):
-        cont += x**i/Factorial(i)
+        cont += (((e**a)/Factorial(i)) * (x - a)**i)
     return cont
 
 def main():
     print(__doc__)
     
     x = int(input("Dame una x: "))
+    a = int(input("Dame una a: "))
     cantidad_terminos = int(input("Dame la cantidad de terminos: "))
     
-    print(Taylor(x, cantidad_terminos))
+    print(Taylor(x, cantidad_terminos, a))
     
 if __name__ == "__main__":
     main()
